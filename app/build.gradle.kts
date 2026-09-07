317ce2a8dfe12352a627248f6da70e0bd4ad3dfe
ciomauri011 <ciomauri011@gmail.com>
2026-09-07 16:39:05 -0500

    Icono PNG exacto + banner TV, versionCode 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.brave.tv.youtube"
         minSdk = 23
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     buildTypes {
